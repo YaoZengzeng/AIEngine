8716e729e2e63ca7b764b99db5835b6989b2727b
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-30 08:42:33 +0000

    adapt kubebuiler

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,33 @@
-FROM golang:1.23.3 AS builder
-
-ARG GO_LDFLAGS=""
+# Build the manager binary
+FROM golang:1.22 AS builder
+ARG TARGETOS
+ARG TARGETARCH
 
 WORKDIR /workspace
-COPY go.mod go.sum ./
-RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg/mod \
-    go mod download
+# Copy the Go Modules manifests
+COPY go.mod go.mod
+COPY go.sum go.sum
+# cache deps before building and copying source so that we don't need to re-download as much
+# and so that source changes don't invalidate our downloaded layer
+RUN go mod download
 
-COPY . ./
-RUN --mount=type=cache,target=/root/.cache/go-build --mount=type=cache,target=/go/pkg/mod \
-    CGO_ENABLED=0  \
-    GOOS=${TARGETOS} \
-    GOARCH=${TARGETARCH} \
-    go build -o /bin/aiengine -ldflags "${GO_LDFLAGS}" .
+# Copy the go source
+COPY cmd/main.go cmd/main.go
+COPY api/ api/
+COPY internal/ internal/
 
-# Need root user for UDS
-FROM gcr.io/distroless/static-debian11
-COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-COPY --from=builder /bin/aiengine /
+# Build
+# the GOARCH has not a default value to allow the binary be built according to the host where the command
+# was called. For example, if we call make docker-build in a local env which has the Apple Silicon M1 SO
+# the docker BUILDPLATFORM arg will be linux/arm64 when for Apple x86 it will be linux/amd64. Therefore,
+# by leaving it empty we can ensure that the container and binary shipped on it will have the same platform.
+RUN CGO_ENABLED=0 GOOS=${TARGETOS:-linux} GOARCH=${TARGETARCH} go build -a -o manager cmd/main.go
 
-ENTRYPOINT ["/aiengine"]
+# Use distroless as minimal base image to package the manager binary
+# Refer to https://github.com/GoogleContainerTools/distroless for more details
+FROM gcr.io/distroless/static:nonroot
+WORKDIR /
+COPY --from=builder /workspace/manager .
+USER 65532:65532
+
+ENTRYPOINT ["/manager"]
---
+++ cmd/main.go
@@ -0,0 +1,390 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	"github.com/tmc/langchaingo/llms"
+	"golang.org/x/time/rate"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	envoy_api_v3_core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_service_proc_v3 "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	v32 "github.com/envoyproxy/go-control-plane/envoy/type/v3"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	aiv1alpha1 "AIEngine/api/v1alpha1"
+	"AIEngine/internal/controller"
+	// +kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+type AIEngine struct {
+	rateLimiter map[string]*rate.Limiter
+}
+
+type Request struct {
+	Model string `json:"model"`
+
+	Prompt string `json:"prompt"`
+}
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(aiv1alpha1.AddToScheme(scheme))
+	// +kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	var secureMetrics bool
+	var enableHTTP2 bool
+	var port int
+	var tlsOpts []func(*tls.Config)
+	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
+		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	flag.BoolVar(&secureMetrics, "metrics-secure", true,
+		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
+	flag.BoolVar(&enableHTTP2, "enable-http2", false,
+		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+	opts := zap.Options{
+		Development: true,
+	}
+
+	flag.IntVar(&port, "port", 9002, "gRPC port")
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	// if the enable-http2 flag is false (the default), http/2 should be disabled
+	// due to its vulnerabilities. More specifically, disabling http/2 will
+	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
+	// Rapid Reset CVEs. For more information see:
+	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
+	// - https://github.com/advisories/GHSA-4374-p667-p6c8
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	if !enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
+	webhookServer := webhook.NewServer(webhook.Options{
+		TLSOpts: tlsOpts,
+	})
+
+	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
+	// More info:
+	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/server
+	// - https://book.kubebuilder.io/reference/metrics.html
+	metricsServerOptions := metricsserver.Options{
+		BindAddress:   metricsAddr,
+		SecureServing: secureMetrics,
+		TLSOpts:       tlsOpts,
+	}
+
+	if secureMetrics {
+		// FilterProvider is used to protect the metrics endpoint with authn/authz.
+		// These configurations ensure that only authorized users and service accounts
+		// can access the metrics endpoint. The RBAC are configured in 'config/rbac/kustomization.yaml'. More info:
+		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/filters#WithAuthenticationAndAuthorization
+		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
+
+		// TODO(user): If CertDir, CertName, and KeyName are not specified, controller-runtime will automatically
+		// generate self-signed certificates for the metrics server. While convenient for development and testing,
+		// this setup is not recommended for production.
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		Metrics:                metricsServerOptions,
+		WebhookServer:          webhookServer,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "1bbf1481.kmesh.net",
+		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+		// when the Manager ends. This requires the binary to immediately end when the
+		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+		// speeds up voluntary leader transitions as the new leader don't have to wait
+		// LeaseDuration time first.
+		//
+		// In the default scaffold provided, the program ends immediately after
+		// the manager stops, so would be fine to enable this option. However,
+		// if you are doing or is intended to do any operation such as perform cleanups
+		// after the manager stops then its usage might be unsafe.
+		// LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controller.AIExtensionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AIExtension")
+		os.Exit(1)
+	}
+	// +kubebuilder:scaffold:builder
+
+	log.Printf("Start AI Engine\n")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	gs := grpc.NewServer()
+	envoy_service_proc_v3.RegisterExternalProcessorServer(gs, &AIEngine{rateLimiter: make(map[string]*rate.Limiter)})
+
+	go func() {
+		err = gs.Serve(lis)
+		if err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+func (s *AIEngine) Process(srv envoy_service_proc_v3.ExternalProcessor_ProcessServer) error {
+	ctx := srv.Context()
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+		req, err := srv.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return status.Errorf(codes.Unknown, "cannot receive stream request: %v", err)
+		}
+
+		resp := &envoy_service_proc_v3.ProcessingResponse{}
+		switch v := req.Request.(type) {
+		case *envoy_service_proc_v3.ProcessingRequest_RequestHeaders:
+			log.Printf("Handle Request Headers\n")
+
+			xrch := ""
+			if v.RequestHeaders != nil {
+				hdrs := v.RequestHeaders.Headers.GetHeaders()
+				for _, hdr := range hdrs {
+					log.Printf("REQUEST HEADER: %s:%s\n", hdr.Key, hdr.Value)
+					if hdr.Key == "x-request-client-header" {
+						xrch = string(hdr.RawValue)
+					}
+				}
+			}
+
+			rhq := &envoy_service_proc_v3.HeadersResponse{
+				Response: &envoy_service_proc_v3.CommonResponse{
+					HeaderMutation: &envoy_service_proc_v3.HeaderMutation{
+						SetHeaders: []*envoy_api_v3_core.HeaderValueOption{
+							{
+								Header: &envoy_api_v3_core.HeaderValue{
+									Key:      "x-request-ext-processed",
+									RawValue: []byte("true"),
+								},
+							},
+						},
+					},
+				},
+			}
+
+			if xrch != "" {
+				rhq.Response.HeaderMutation.SetHeaders = append(rhq.Response.HeaderMutation.SetHeaders,
+					&envoy_api_v3_core.HeaderValueOption{
+						Header: &envoy_api_v3_core.HeaderValue{
+							Key:      "x-request-client-header",
+							RawValue: []byte("mutated"),
+						},
+					})
+				rhq.Response.HeaderMutation.SetHeaders = append(rhq.Response.HeaderMutation.SetHeaders,
+					&envoy_api_v3_core.HeaderValueOption{
+						Header: &envoy_api_v3_core.HeaderValue{
+							Key:      "x-request-client-header-received",
+							RawValue: []byte(xrch),
+						},
+					})
+			}
+
+			resp = &envoy_service_proc_v3.ProcessingResponse{
+				Response: &envoy_service_proc_v3.ProcessingResponse_RequestHeaders{
+					RequestHeaders: rhq,
+				},
+			}
+			break
+		case *envoy_service_proc_v3.ProcessingRequest_RequestBody:
+			log.Printf("Handle Request Body")
+
+			rbq := &envoy_service_proc_v3.BodyResponse{
+				Response: &envoy_service_proc_v3.CommonResponse{},
+			}
+
+			resp = &envoy_service_proc_v3.ProcessingResponse{
+				Response: &envoy_service_proc_v3.ProcessingResponse_RequestBody{
+					RequestBody: rbq,
+				},
+			}
+
+			if v.RequestBody != nil {
+				body := v.RequestBody.GetBody()
+				log.Printf("Request BODY: %s", string(body))
+
+				var req Request
+
+				err := json.Unmarshal(body, &req)
+				if err != nil {
+					log.Printf("Failed to unmarshal req")
+					break
+				}
+
+				if _, ok := s.rateLimiter[req.Model]; !ok {
+					s.rateLimiter[req.Model] = rate.NewLimiter(rate.Every(time.Minute), 10)
+				}
+
+				tokenCount := llms.CountTokens("", req.Prompt)
+				log.Printf("Token Count is %d", tokenCount)
+
+				if allowed := s.rateLimiter[req.Model].AllowN(time.Now(), tokenCount); !allowed {
+					log.Printf("Trigger Rate Limit")
+					resp = &envoy_service_proc_v3.ProcessingResponse{
+						Response: &envoy_service_proc_v3.ProcessingResponse_ImmediateResponse{
+							ImmediateResponse: &envoy_service_proc_v3.ImmediateResponse{
+								Status: &v32.HttpStatus{Code: v32.StatusCode_TooManyRequests},
+							},
+						},
+					}
+				}
+			}
+			break
+		case *envoy_service_proc_v3.ProcessingRequest_RequestTrailers:
+			log.Printf("Handle Request Trailers")
+			break
+		case *envoy_service_proc_v3.ProcessingRequest_ResponseHeaders:
+			log.Printf("Handle Response Headers")
+
+			if v.ResponseHeaders != nil {
+				hdrs := v.ResponseHeaders.Headers.GetHeaders()
+				for _, hdr := range hdrs {
+					log.Printf("RESPONSE HEADER: %s:%s\n", hdr.Key, hdr.Value)
+				}
+			}
+
+			rhq := &envoy_service_proc_v3.HeadersResponse{
+				Response: &envoy_service_proc_v3.CommonResponse{
+					HeaderMutation: &envoy_service_proc_v3.HeaderMutation{
+						SetHeaders: []*envoy_api_v3_core.HeaderValueOption{
+							{
+								Header: &envoy_api_v3_core.HeaderValue{
+									Key:      "x-response-ext-processed",
+									RawValue: []byte("true"),
+								},
+							},
+						},
+					},
+				},
+			}
+			resp = &envoy_service_proc_v3.ProcessingResponse{
+				Response: &envoy_service_proc_v3.ProcessingResponse_ResponseHeaders{
+					ResponseHeaders: rhq,
+				},
+			}
+			break
+		case *envoy_service_proc_v3.ProcessingRequest_ResponseBody:
+			log.Printf("Handle Response Body")
+
+			if v.ResponseBody != nil {
+				body := v.ResponseBody.GetBody()
+				log.Printf("Response BODY: %s", string(body))
+			}
+
+			rbq := &envoy_service_proc_v3.BodyResponse{
+				Response: &envoy_service_proc_v3.CommonResponse{},
+			}
+
+			resp = &envoy_service_proc_v3.ProcessingResponse{
+				Response: &envoy_service_proc_v3.ProcessingResponse_ResponseBody{
+					ResponseBody: rbq,
+				},
+			}
+			break
+		case *envoy_service_proc_v3.ProcessingRequest_ResponseTrailers:
+			log.Printf("Handle Response Trailers")
+			break
+		default:
+			log.Printf("Unknown Request type %v\n", v)
+		}
+		if err := srv.Send(resp); err != nil {
+			log.Printf("send error %v", err)
+		}
+	}
+}
